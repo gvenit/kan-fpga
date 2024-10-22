531ceba696722074f55639afd770a3912481e3a3
gvenit <venitourakis.george@gmail.com>
2024-10-22 13:45:34 +0300

    LinearProcessingElement first approach

--- rtl/LinearProcessingElement.v
+++ rtl/LinearProcessingElement.v
@@ -19,8 +19,10 @@
  */
 
 module LinearProcessingElement #(
-    // Number of PEs in Processing Array 
-    parameter PE_NUMBER = 8,
+    // Number of PEs in Processing Array i axis
+    parameter PE_NUMBER_I = 4,
+    // Number of PEs in Processing Array j axis
+    parameter PE_NUMBER_J = 4,
     // Position of current PE in the i axis
     parameter PE_POSITION_I = 0,
     // Position of current PE in the j axis
@@ -48,7 +50,7 @@ module LinearProcessingElement #(
     // Propagate tuser signal
     parameter USER_ENABLE = 1,
     // tuser signal width
-    parameter USER_WIDTH = 8,
+    parameter USER_WIDTH = 2 + $clog2(PE_NUMBER_J),
     // Output Destination 
     parameter OUTPUT_DEST = 1;
     // Output Destination 
@@ -57,10 +59,14 @@ module LinearProcessingElement #(
     parameter DATA_WIDTH_U_D = (DATA_WIDTH_OP1 > DATA_WIDTH_RSLT) ? DATA_WIDTH_OP1 : DATA_WIDTH_RSLT,
     // Resolve Left/Right Bit Width
     parameter DATA_WIDTH_L_R = DATA_WIDTH_DATA,
-    // Operator 1 Mask
+    // Operator 1 User Mask
     parameter OP1_USER_MASK = USER_WIDTH'h40,
-    // Result Mask
-    parameter RSLT_USER_MASK = USER_WIDTH'h80
+    // Result User Mask
+    parameter RSLT_USER_MASK = USER_WIDTH'h80,
+    // Result ID
+    parameter RSLT_ID = ID_WIDTH'h01,
+    // Result Destination
+    parameter RSLT_DEST = DEST_WIDTH'h00
 ) (
   input                             clk,
   input                             rst,
@@ -114,6 +120,24 @@ module LinearProcessingElement #(
    */
   output err_unalligned_data;
 );
+  // FSM States
+  localparam FSM_STATE_WIDTH = 3;
+  localparam FSM_STRT = FSM_STATE_WIDTH'd0;
+  localparam FSM_STRL = FSM_STATE_WIDTH'd1;
+  localparam FSM_STRU = FSM_STATE_WIDTH'd2;
+  localparam FSM_MAC  = FSM_STATE_WIDTH'd3;
+  localparam FSM_ERR  = FSM_STATE_WIDTH'd4;
+  localparam FSM_END  = FSM_STATE_WIDTH'd5;
+
+  // DataFlow Local Parameters
+  localparam MAC_OP_SIZE  = (DATA_WIDTH_RSLT > DATA_WIDTH_OP0 + DATA_WIDTH_SCALE) ? DATA_WIDTH_RSLT : DATA_WIDTH_OP0 + DATA_WIDTH_SCALE;
+  localparam MLT_OP_SIZE  = DATA_WIDTH_OP0 + DATA_WIDTH_SCALE;
+  localparam MAC_RSLT_LSB = FRACTIONAL_BITS_DATA + FRACTIONAL_BITS_SCALE - FRACTIONAL_BITS_RSLT;
+  localparam MAC_RSLT_MSB = RSLT_LSB + DATA_WIDTH_RSLT - 1;
+
+  // Output Control Local Parameters
+  localparam PE_J_WIDTH = $clog2(PE_NUMBER_J);
+
   // Up AXI-Stream internal signals
   wire [DATA_WIDTH_U_D-1:0] int_axis_up_tdata;
   wire                      int_axis_up_tvalid;
@@ -153,49 +177,49 @@ module LinearProcessingElement #(
   // FSM input signals
   wire in_handshake_up   = s_axis_up_tvalid && s_axis_up_tready;
   wire in_handshake_left = s_axis_left_tvalid && s_axis_left_tready;
-  wire op_retry = !(int_axis_left_tvalid && int_axis_up_tvalid && (int_axis_up_tuser & OP1_USER_MASK));
 
-  wire err_user_flag_int = int_axis_up_tvalid && ^(int_axis_up_tuser & (OP1_USER_MASK | RSLT_USER_MASK));
+  wire operand_flag = int_axis_up_tvalid && |(int_axis_up_tuser & OP1_USER_MASK);
+  wire result_flag  = int_axis_up_tvalid && |(int_axis_up_tuser & RSLT_USER_MASK);
+
+  wire op_retry = !(int_axis_left_tvalid && operand_flag);
+
+  wire partial_sum_last_reg_next = (!op_retry) && (int_axis_left_tlast & int_axis_up_tlast)
+
+  wire err_user_flag_int = operand_flag && result_flag;
   wire err_unalligned_data_int = !(op_retry || !(int_axis_left_tlast ^ int_axis_up_tlast)) ;
 
   // FSM Output Signals
   reg store_l, store_u;
   reg forward_l, forward_u;
   reg push_d;
+  reg [PE_J_WIDTH-1:0] timer; // To keep track that all nodes above have transfered their results first
 
   reg op_start;
   reg bypass_adder;
-
-  // FSM States
-  localparam FSM_STATE_WIDTH = 3;
-  localparam FSM_STRT = FSM_STATE_WIDTH'd0;
-  localparam FSM_STRL = FSM_STATE_WIDTH'd1;
-  localparam FSM_STRU = FSM_STATE_WIDTH'd2;
-  localparam FSM_MAC  = FSM_STATE_WIDTH'd3;
-  localparam FSM_ERR  = FSM_STATE_WIDTH'd4;
-  localparam FSM_END  = FSM_STATE_WIDTH'd5;
 
   // FSM Registers & Wires
   reg  [FSM_STATE_WIDTH-1:0] fsm_state = FSM_START;
   wire [FSM_STATE_WIDTH-1:0] fsm_state_next;
 
-  // DataFlow Local Parameters
-  localparam MAC_OP_SIZE  = (DATA_WIDTH_RSLT > DATA_WIDTH_OP0 + DATA_WIDTH_SCALE) ? DATA_WIDTH_RSLT : DATA_WIDTH_OP0 + DATA_WIDTH_SCALE;
-  localparam MLT_OP_SIZE  = DATA_WIDTH_OP0 + DATA_WIDTH_SCALE;
-  localparam MAC_RSLT_LSB = FRACTIONAL_BITS_DATA + FRACTIONAL_BITS_SCALE - FRACTIONAL_BITS_RSLT;
-  localparam MAC_RSLT_MSB = RSLT_LSB + DATA_WIDTH_RSLT - 1;
-
   // DataFlow Registers & Wires
+  reg                   partial_sum_last_reg = 1'b0;
   reg [MAC_OP_SIZE-1:0] partial_sum_reg = 0, partial_sum_reg_next, partial_sum_fb, partial_sum_rslt;
   reg [MLT_OP_SIZE-1:0] mult_res;
 
-  wire [DATA_WIDTH_OP0-1:] mult_op0;
-  wire [DATA_WIDTH_OP1-1:] mult_op1;
+  wire [DATA_WIDTH_OP0-1:0] mult_op0;
+  wire [DATA_WIDTH_OP1-1:0] mult_op1;
+
+  // Output Control Registers & Wires
+  wire export_rslt;
+  wire out_handshake_down = m_axis_down_tvalid && m_axis_down_tready;
+
+  wire out_handshake_right = m_axis_right_tvalid && m_axis_right_tready;
 
   // FSM Logic
   always @(posedge clk) begin
     if (rst) begin
       fsm_state <= FSM_START;
+      partial_sum_last_reg <= 1'b0;
       
       // Freeze all data flows
       store_l      <= 1'b0;
@@ -211,7 +235,10 @@ module LinearProcessingElement #(
 
       // Reset Errors
       err_unalligned_data <= 1'b0;
-      err_user_flag <= 1'b0
+      err_user_flag <= 1'b0;
+
+      // Reset Timer
+      timer <= PE_NUMBER_J;
 
     end else begin
       fsm_state <= fsm_state_next;
@@ -231,6 +258,7 @@ module LinearProcessingElement #(
           // Reset Partial Sum
           op_start     <= 1'b1;
           bypass_adder <= 1'b1;
+          partial_sum_last_reg <= 1'b0;
 
           // No Errors
           err_unalligned_data <= 1'b0;
@@ -250,6 +278,7 @@ module LinearProcessingElement #(
           // Lock Partial Sum
           op_start     <= 1'b0;
           bypass_adder <= 1'b1;
+          partial_sum_last_reg <= 1'b0;
 
           // No Errors
           err_unalligned_data <= 1'b0;
@@ -269,6 +298,7 @@ module LinearProcessingElement #(
           // Lock Partial Sum
           op_start     <= 1'b0;
           bypass_adder <= 1'b1;
+          partial_sum_last_reg <= 1'b0;
 
           // No Errors
           err_unalligned_data <= 1'b0;
@@ -288,6 +318,7 @@ module LinearProcessingElement #(
           // Update Partial Sum
           op_start     <= 1'b0;
           bypass_adder <= 1'b0;
+          partial_sum_last_reg <= partial_sum_last_reg_next;
 
           // No Errors
           err_unalligned_data <= 1'b0;
@@ -307,10 +338,15 @@ module LinearProcessingElement #(
           // Lock Partial Sum
           op_start     <= 1'b0;
           bypass_adder <= 1'b1;
+          partial_sum_last_reg <= partial_sum_last_reg; // Do not update until result is transmitted
 
           // No Errors
           err_unalligned_data <= 1'b0;
           err_user_flag <= 1'b0;
+
+          if (timer && result_flag && ) begin
+            
+          end
 
         FSM_ERR :
           // Return & Forward Up
@@ -326,6 +362,7 @@ module LinearProcessingElement #(
           // Lock Partial Sum
           op_start     <= 1'b0;
           bypass_adder <= 1'b1;
+          partial_sum_last_reg <= partial_sum_last_reg; 
 
           // Sample Errors
           err_unalligned_data <= err_unalligned_data_int;
@@ -337,11 +374,16 @@ module LinearProcessingElement #(
     end
   end
 
+  assign export_rslt = push_d && (!timer);
+
   // FSM Next State Logic
   always @(*) begin
     case (fsm_state)
       FSM_START :
         if (in_handshake_left && in_handshake_up) begin
+          if (partial_sum_last_reg_next) begin
+            fsm_state_next <= FSM_END;
+          end
           fsm_state_next <= FSM_MAC;
         end else if(in_handshake_left) begin
           fsm_state_next <= FSM_STRL;
@@ -374,6 +416,11 @@ module LinearProcessingElement #(
           fsm_state_next <= FSM_STRU;
         end
 
+      FSM_END :
+        if (export_rslt && out_handshake_down) begin
+          fsm_state_next <= FSM_STRT;  
+        end
+        
       FSM_ERR :
         fsm_state_next <= FSM_STRT;
 
@@ -387,6 +434,8 @@ module LinearProcessingElement #(
   end
 
   // Up AXI-Stream Skid Buffer
+  wire s_axis_up_tready_int;
+  assign s_axis_up_tready = s_axis_up_tready_int & store_u;
   axis_register #(
     // Width of AXI stream interfaces in bits
     .DATA_WIDTH(DATA_WIDTH_U_D),
@@ -416,8 +465,8 @@ module LinearProcessingElement #(
     .rst(rst),
     .s_axis_tdata(s_axis_up_tdata),
     .s_axis_tkeep(s_axis_up_tkeep),
-    .s_axis_tvalid(s_axis_up_tvalid),
-    .s_axis_tready(s_axis_up_tready),
+    .s_axis_tvalid(s_axis_up_tvalid & store_u),
+    .s_axis_tready(s_axis_up_tready_int),
     .s_axis_tlast(s_axis_up_tlast),
     .s_axis_tid(s_axis_up_tid),
     .s_axis_tdest(s_axis_up_tdest),
@@ -433,6 +482,8 @@ module LinearProcessingElement #(
   );
 
   // Left AXI-Stream Skid Buffer
+  wire s_axis_left_tready_int;
+  assign s_axis_left_tready = s_axis_left_tready_int & store_l;
   axis_register #(
     // Width of AXI stream interfaces in bits
     .DATA_WIDTH(DATA_WIDTH_L_R),
@@ -461,15 +512,15 @@ module LinearProcessingElement #(
     .clk(clk),
     .rst(rst),
     .s_axis_tdata(s_axis_left_tdata),
-    .s_axis_tkeep(s_axis_left_tkeep),
-    .s_axis_tvalid(s_axis_left_tvalid),
-    .s_axis_tready(s_axis_left_tready),
+    .s_axis_tkeep(1'b1),
+    .s_axis_tvalid(s_axis_left_tvalid & store_l),
+    .s_axis_tready(s_axis_left_tready_int),
     .s_axis_tlast(s_axis_left_tlast),
     .s_axis_tid(s_axis_left_tid),
     .s_axis_tdest(s_axis_left_tdest),
     .s_axis_tuser(s_axis_left_tuser),
     .m_axis_tdata(int_axis_left_tdata),
-    .m_axis_tkeep(int_axis_left_tkeep),
+    .m_axis_tkeep(),
     .m_axis_tvalid(int_axis_left_tvalid),
     .m_axis_tready(int_axis_left_tready),
     .m_axis_tlast(int_axis_left_tlast),
@@ -487,5 +538,23 @@ module LinearProcessingElement #(
 
   assign partial_sum_fb = (op_start) ? MAC_OP_SIZE'0 : partial_sum_reg;
   assign partial_sum_reg_next = (bypass_adder) ? partial_sum_fb : partial_sum_rslt;
+
+  // Output Right AXI-Stream
+  assign m_axis_right_tdata   = int_axis_left_tdata;
+  assign m_axis_right_tvalid  = int_axis_left_tvalid  & forward_l;
+  assign int_axis_left_tready = m_axis_right_tready & forward_l;
+  assign m_axis_right_tlast   = int_axis_left_tlast;
+  assign m_axis_right_tid     = int_axis_left_tid;
+  assign m_axis_right_tdest   = int_axis_left_tdest;
+  assign m_axis_right_tuser   = int_axis_left_tuser;
+
+  // Output Down AXI-Stream
+  assign m_axis_down_tdata   = (export_rslt) ? partial_sum_reg      : int_axis_up_tdata;
+  assign m_axis_down_tvalid  = (export_rslt) ? 1'b1                 : int_axis_up_tvalid  & forward_l;
+  assign int_axis_up_tready  = (export_rslt) ? 1'b0                 : m_axis_down_tready & forward_l;
+  assign m_axis_down_tlast   = (export_rslt) ? partial_sum_last_reg : int_axis_up_tlast;
+  assign m_axis_down_tid     = (export_rslt) ? RSLT_ID              : int_axis_up_tid;
+  assign m_axis_down_tdest   = (export_rslt) ? RSLT_DEST            : int_axis_up_tdest;
+  assign m_axis_down_tuser   = (export_rslt) ? PE_NUMBER_J          : int_axis_up_tuser;
 
 endmodule
